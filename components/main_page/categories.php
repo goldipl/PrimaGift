1ea173a9414378c30c35689e37bef12d1ab27b13
goldipl <mgodlewski92@gmail.com>
2025-04-06 16:48:31 +0200

    Add styles for main page categories and update hero section height
    - Implemented a new SCSS file for main page categories with responsive design, hover effects, and category slot styling.
    - Adjusted the hero open section to have an automatic height for better responsiveness on smaller screens.

--- components/main_page/categories.php
+++ components/main_page/categories.php
@@ -1,3 +1,48 @@
 <section id="main-page-categories">
-    <div class="main-page-categories container"></div>
+    <div class="main-page-categories container">
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/umbrella.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Parasole</span>
+        </a>
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/thermical-mugs.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Kubki termiczne</span>
+        </a>
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/powerbanks.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Powerbanki</span>
+        </a>
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/pens.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Długopisy</span>
+        </a>
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/shopping-bags.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Torby na zakupy</span>
+        </a>
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/pendrive.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Pendrive</span>
+        </a>
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/eco-gadgets.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Gadżety ekologiczne</span>
+        </a>
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/tools.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Narzędzia</span>
+        </a>
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/children-gadgets.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Gadżety dla dzieci</span>
+        </a>
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/antistress.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Antystresy</span>
+        </a>
+        <a href="" class="main-page-categories__slot">
+            <img class="category-img" src="./assets/icons/common/categories/new.svg" alt="ikonka" width="50" height="50">
+            <span class="category-label">Nowości</span>
+        </a>
+    </div>
 </section>
