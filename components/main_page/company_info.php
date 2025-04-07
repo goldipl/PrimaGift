176eba6fbf57e4621adb6aee257e5de5f0bf5dc7
goldipl <mgodlewski92@gmail.com>
2025-04-07 17:52:33 +0200

    Add company info section styles to main SCSS
    - Imported new _company_info.scss file into main.scss
    - Created styles for company info section including layout, typography, and responsive design
    - Added styles for company info text, buttons, and images with hover effects

--- components/main_page/company_info.php
+++ components/main_page/company_info.php
@@ -1,3 +1,35 @@
 <section id="company-info">
-    <div class="company-info container"></div>
+    <div class="company-info container">
+        <div class="company-info__text">
+            <h3>Wiemy jak ważny jest wizerunek firmy</h3>
+            <p>Pomożemy Ci w jego kreowaniu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi condimentum turpis eu ligula dapibus euismod. Nam ut ex a nulla pellentesque interdum id sodales nisi. Phasellus accumsan interdum elit nec lacinia. Ut mattis sit amet odio in blandit. Nullam non luctus ipsum. Quisque et semper dolor. Aenean ultricies dolor sit amet lectus ullamcorper, sit amet gravida quam fringilla. Pellentesque eu commodo quam. Maecenas vel eleifend massa</p>
+            <a href="" class="company-info__text--btn">dowiedz się więcej</a>
+        </div>
+        <div class="company-info__image">
+            <img src="./assets/img/main_page/sections/mockup.png" alt="Prima Gift Produkty" width="906" height="593" class="company-info__image--img">
+            <a href="" class="company-info__image--link how-to-buy">
+                <div class="link-img-box">
+                    <div class="link-img-box__image">
+                        <img src="./assets/icons/sections/how-to-buy.svg" alt="Prima Gift Produkty" width="55" height="62" class="link-img-box__image--img">
+                    </div>
+                    <div class="link-img-box__text">
+                        <h4>Jak kupować</h4>
+                        <p>Zamów gadżety w szybki i prosty sposób!
+                        Kliknij tutaj i zobacz instrukcję krok po kroku!</p>
+                    </div>
+                </div>
+            </a>
+            <a href="" class="company-info__image--link branding-techniques">
+                <div class="link-img-box">
+                    <div class="link-img-box__image">
+                        <img src="./assets/icons/sections/branding-techniques.svg" alt="Prima Gift Produkty" width="56" height="60" class="link-img-box__image--img">
+                    </div>
+                    <div class="link-img-box__text">
+                        <h4>Techniki znakowania</h4>
+                        <p>Oferujemy wszelkie możliwe techniki znakowania dostępne na rynku. Zobacz szczegóły!</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
 </section>
