efedd8c067fbe3ce743a0408565ff1c2da4546a7
goldipl <mgodlewski92@gmail.com>
2025-04-07 17:14:06 +0200

    Add gadget idea section styles and import in main SCSS
    - Created a new SCSS file for the gadget idea section (_gadget_idea.scss) with styles for layout, text, and button.
    - Imported the new gadget idea styles into the main SCSS file (main.scss).

--- components/main_page/gadget_idea.php
+++ components/main_page/gadget_idea.php
@@ -1,3 +1,13 @@
 <section id="gadget-idea">
-    <div class="gadget-idea container"></div>
+    <div class="gadget-idea container">
+        <div class="gadget-idea__image">
+            <img src="./assets/img/main_page/sections/shop-bag.png" alt="Gadżety" width="906" height="593" class="gadget-idea__image--img">
+        </div>
+        <div class="gadget-idea__text">
+            <h3>Masz pomysł na własny gadżet?</h3>
+            <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi condimentum turpis eu ligula dapibus euismod. Nam ut ex a nulla pellentesque interdum id sodales nisi</strong></p>
+            <p>Phasellus accumsan interdum elit nec lacinia. Ut mattis sit amet odio in blandit. Nullam non luctus ipsum. Quisque et semper dolor. Aenean ultricies dolor sit amet lectus ullamcorper, sit amet gravida quam fringilla. Pellentesque eu commodo quam.</p>
+            <a href="" class="gadget-idea__text--btn">skontaktuj się z nami</a>
+        </div>
+    </div>
 </section>
