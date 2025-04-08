663e702cb4180f92d11ad28825313775946baf7d
goldipl <mgodlewski92@gmail.com>
2025-04-08 11:20:41 +0200

    Add our strengths section styles
    - Imported new SCSS file for the "Our Strengths" section into main.scss.
    - Created _our_strenghts.scss with styles for the section, including responsive design for various screen sizes.
    - Defined layout and styling for strengths blocks, icons, and text within the section.

--- components/main_page/our_strengths.php
+++ components/main_page/our_strengths.php
@@ -1,3 +1,52 @@
 <section id="our-strengths">
-    <div class="our-strengths container"></div>
+    <div class="our-strengths container">
+        <h2>Nasze mocne strony</h2>
+        <div class="strengths-blocks">
+            <div class="strengths-blocks__slot">
+                <div class="strengths-blocks__slot__icon">
+                    <img src="./assets/icons/sections/free-delivery-from-1500.svg" alt="darmowa dostawa od 1500 zł" width="60" height="60" class="strengths-blocks__slot__icon--img"/>
+                    <img src="./assets/icons/common/pink-lines.svg" alt="różowe linie" width="66" height="50" class="strengths-blocks__slot__icon--lines"/>
+                </div>
+                <div class="strengths-blocks__slot__text">
+                    <span>darmowa dostawa od 1500 zł</span>
+                </div>
+            </div>
+            <div class="strengths-blocks__slot">
+                <div class="strengths-blocks__slot__icon">
+                    <img src="./assets/icons/sections/many-products.svg" alt="szeroka gama oferowanych produktów" width="60" height="60" class="strengths-blocks__slot__icon--img" />
+                    <img src="./assets/icons/common/pink-lines.svg" alt="różowe linie" width="66" height="50" class="strengths-blocks__slot__icon--lines"/>
+                </div>
+                <div class="strengths-blocks__slot__text">
+                    <span>szeroka gama oferowanych produktów</span>
+                </div>
+            </div>
+            <div class="strengths-blocks__slot">
+                <div class="strengths-blocks__slot__icon">
+                    <img src="./assets/icons/sections/fast-realization.svg" alt="ekspresowa realizacja" width="60" height="60" class="strengths-blocks__slot__icon--img" />
+                    <img src="./assets/icons/common/pink-lines.svg" alt="różowe linie" width="66" height="50" class="strengths-blocks__slot__icon--lines"/>
+                </div>
+                <div class="strengths-blocks__slot__text">
+                    <span>ekspresowa realizacja</span>
+                </div>
+            </div>
+            <div class="strengths-blocks__slot">
+                <div class="strengths-blocks__slot__icon">
+                    <img src="./assets/icons/sections/free-preview.svg" alt="bezpłatna wizualizacja" width="60" height="60" class="strengths-blocks__slot__icon--img"/>
+                </div>
+                <div class="strengths-blocks__slot__text">
+                    <span>bezpłatna wizualizacja</span>
+                </div>
+            </div>
+            <div class="strengths-blocks__slot">
+                <div class="strengths-blocks__slot__icon">
+                    <img src="./assets/icons/sections/hundreds-of-happy-clients.svg" alt="setki udanych realizacji" width="60" height="60" class="strengths-blocks__slot__icon--img" />
+                    <img src="./assets/icons/common/pink-lines.svg" alt="różowe linie" width="66" height="50" class="strengths-blocks__slot__icon--lines"/>
+                </div>
+                <div class="strengths-blocks__slot__text">
+                    <span>setki udanych realizacji</span>
+                </div>
+            </div>
+        </div>
+        <img src="./assets/icons/sections/white-rectangle.svg" alt="biały prostokąt" class="our-strengths__white-shape" width="1495" height="199"/>
+    </div>
 </section>
