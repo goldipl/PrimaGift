65d96fc6094ab479acc4b32f9e1f41a89f9c4838
goldipl <mgodlewski92@gmail.com>
2025-04-09 19:43:21 +0200

    fill and styling category page with pagination and filters

--- components/category/category_top_filters.php
+++ components/category/category_top_filters.php
@@ -1,30 +1,54 @@
 <div class="category-top-filters">
-    <div class="category-top-filters__slot">
-        <label for="sort-by-availability">Dostępność:</label>
-        <select name="sort-by-availability" id="sort-by-availability" class="form-select">
-            <option value="Dostepny">Dostepny</option>
-            <option value="Niedostępny">Niedostępny</option>
-        </select>
+    <div class="category-top-filters-upper">
+        <div class="category-top-filters__slot">
+            <h2>Filtruj produkty:</h2>
+        </div>
+        <div class="category-top-filters__slot">
+            <label for="sort-by-name">Nazwa:</label>
+            <input name="sort-by-name" id="sort-by-name" class="form-input" placeholder="Wpisz nazwę lub kod produktu"></input>
+        </div>
+        <div class="category-top-filters__slot">
+            <label for="sort-by-color">Kolor:</label>
+            <select name="sort-by-color" id="sort-by-color" class="form-select">
+                <option value="Biały">Biały</option>
+                <option value="Czarny">Czarny</option>
+            </select>
+        </div>
+        <div class="category-top-filters__slot">
+            <label for="sort-by-price-from">Cena od:</label>
+            <input name="sort-by-price-from" id="sort-by-price-from" class="form-input"></input>
+        </div>
+        <div class="category-top-filters__slot">
+            <label for="sort-by-price-to">Cena do:</label>
+            <input name="sort-by-price-to" id="sort-by-price-to" class="form-input" ></input>
+        </div>
+        <div class="category-top-filters__slot">
+            <button class="set-filters">Zastosuj filtry</button>
+        </div>
     </div>
-    <div class="category-top-filters__slot">
-        <label for="sort-by-price">Sortuj wg cena:</label>
-        <select name="sort-by-price" id="sort-by-price" class="form-select">
-            <option value="od 20zł do 40zł">od 20zł do 40zł</option>
-            <option value="od 41zł do 80zł">od 41zł do 80zł</option>
-        </select>
-    </div>
-    <div class="category-top-filters__slot">
-        <label for="sort-by">Sortuj wg:</label>
-        <select name="sort-by" id="sort-by" class="form-select">
-            <option value="Nazwa produktu A-Z">Nazwa produktu A-Z</option>
-            <option value="Nazwa produktu Z-A">Nazwa produktu Z-A</option>
-        </select>
-    </div>
-    <div class="category-top-filters__slot">
-        <label for="show-quantity">Wyświetl:</label>
-        <select name="show-quantity" id="show-quantity" class="form-select">
-            <option value="16">16</option>
-            <option value="32">32</option>
-        </select>
+    <div class="category-top-filters-bottom">
+        <div class="bottom-filters-left">
+            <span class="active-filters-text">Aktywne filtry:</span>
+            <div class="active-filters">
+                <div class="active-filters__slot">
+                    <img src="./assets/icons/common/pink-x.svg" alt="Usuń" width="7" height="7">
+                    <span>20 zł</span>
+                </div>
+                <div class="active-filters__slot">
+                    <img src="./assets/icons/common/pink-x.svg" alt="Usuń" width="7" height="7">
+                    <span>Przykładowy filtr</span>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-filters-right">
+            <div class="category-top-filters__slot">
+                <label for="sort-by-sth">Sortuj według:</label>
+                <select name="sort-by-sth" id="sort-by-sth" class="form-select">
+                    <option value="Wybór 1">Wybór 1</option>
+                    <option value="Wybór 2">Wybór 2</option>
+                </select>
+            </div>
+            <?php include "./components/common/pagination.php"; ?>
+        </div>
     </div>
 </div>
