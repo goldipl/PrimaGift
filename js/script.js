30556ee16b7396dd1ce475da9e122b0357c1162d
goldipl <mgodlewski92@gmail.com>
2025-04-10 09:15:29 +0200

    correct footer year script

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function scrollToTop() {
   });
 }
 
-const footer_year = document.querySelector('.footer__container__bottom span');
+const footer_year = document.querySelector('.footer-container__bottom span');
 
 footer_year.innerHTML = new Date().getFullYear();
 
