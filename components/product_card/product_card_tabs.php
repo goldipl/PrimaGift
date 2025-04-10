70bcfef27f242ff6a142930fec01cc651e21151f
goldipl <mgodlewski92@gmail.com>
2025-04-10 20:10:45 +0200

    Refactor product card styles: enhance tab layout, adjust spacing, and improve responsive design
    - Updated the layout of product card tabs for better alignment and spacing.
    - Modified styles for nav buttons and ask-about-product section to improve usability.
    - Adjusted tab content styles, including padding and background colors for better readability.
    - Improved responsive design for various elements, ensuring better display on smaller screens.
    - Refined typography settings across different text elements for consistency and clarity.
    - Enhanced product gallery styles, including margin adjustments and responsive behavior.

--- components/product_card/product_card_tabs.php
+++ components/product_card/product_card_tabs.php
@@ -1,50 +1,58 @@
 <div class="product_card_tabs container">
-    <nav>
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <button class="nav-link active" id="nav-desc-tab" data-bs-toggle="tab" data-bs-target="#nav-desc" type="button" role="tab" aria-controls="nav-desc" aria-selected="true">Opis produktu</button>
-            <button class="nav-link" id="nav-specification-tab" data-bs-toggle="tab" data-bs-target="#nav-specification" type="button" role="tab" aria-controls="nav-specification" aria-selected="false">Specyfikacja</button>
-            <button class="nav-link" id="nav-detdesc-tab" data-bs-toggle="tab" data-bs-target="#nav-detdesc" type="button" role="tab" aria-controls="nav-detdesc" aria-selected="false">Dostępność</button>
-        </div>
-    </nav>
-    <div class="tab-content" id="nav-tabContent">
-        <div class="tab-pane fade show active" id="nav-desc" role="tabpanel" aria-labelledby="nav-desc-tab" tabindex="0">
-            <p>Klasyczny długopis metalowy z chromowanymi elementami i smukłym klipsem. Długopis wyposażony w duży niebieski wkład.</p>
-        </div>
-        <div class="tab-pane fade" id="nav-specification" role="tabpanel" aria-labelledby="nav-specification-tab" tabindex="0">
-            <div class="table-container short-rows-table">
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Kolor</td>
-                            <td>jasnozielony</td>
-                        </tr>
-                        <tr>
-                            <td>Materiał</td>
-                            <td>aluminium, metal</td>
-                        </tr>
-                        <tr>
-                            <td>Rozmiar produktu</td>
-                            <td>13,6 x Ø 1 cm</td>
-                        </tr>
-                        <tr>
-                            <td>Waga</td>
-                            <td>0,016 kg</td>
-                        </tr>
-                        <tr>
-                            <td>Kraj pochodzenia</td>
-                            <td>Chiny</td>
-                        </tr>
-                        <tr>
-                            <td>Numer celny</td>
-                            <td>96081092000</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="tab-pane fade" id="nav-detdesc" role="tabpanel" aria-labelledby="nav-detdesc-tab" tabindex="0">
-            <p class="green">Dostępny: <span>1080sztuk</span></p> 
-            <small>na zamówienie w ciągu 3-10 dni</small>
-        </div>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <div class="nav-buttons">
+        <button class="nav-link active" id="nav-desc-tab" data-bs-toggle="tab" data-bs-target="#nav-desc" type="button" role="tab" aria-controls="nav-desc" aria-selected="true">Opis produktu</button>
+        <button class="nav-link" id="nav-opinions-tab" data-bs-toggle="tab" data-bs-target="#nav-opinions" type="button" role="tab" aria-controls="nav-opinions" aria-selected="false">Opinie o produkcie</button>
     </div>
+    <a href="" class="ask-about-product">
+        <img src="./assets/icons/common/ask-about-product-icon.svg" alt="zapytaj o produkt" width="26" height="23">
+        <span>zapytaj o produkt</span>
+    </a>
+  </div>
+  <div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active" id="nav-desc" role="tabpanel" aria-labelledby="nav-desc-tab" tabindex="0">
+      <p>Bransoletka sportowa ACTIVE to kolejna propozycja świetnego gadżetu dla aktywnych fizycznie. Wyposażona jest w całą masę przydatnych funkcji, począwszy od krokomierza, przebytego dystansu i spalonych kalorii, poprzez pulsometr, pomiar ciśnienia krwi, test SpO2*, aż po monitoring snu. Aby wszystkie funkcje były dostępne, potrzebny jest smartfon z systemem Android 4.2 lub iOS 8.0 i nowszym, wersja bluetooth min. 4.0 oraz aplikacja o nazwie Lefun Health. Pozostałe funkcje: przypomnienie o braku ruchu, przypomnienie o konieczności uzupełniania płynów, funkcja anti-lost, powiadomienia o wiadomościach i połączeniu, potrząśnij, aby zrobić zdjęcie**, znajdź bransoletkę.</p>
+      <p>* test saturacji – nasycenia hemoglobiny tlenem</p>
+      <p>** funkcja działa tylko poprzez aplikację Lefun</p>
+      <p>UWAGA: Aby połączyć bransoletkę z aplikacją, wymagane jest uruchomienie lokalizacji GPS w telefonie.</p>
+      <p>Aby naładować bransoletkę należy odczepić odpowiednią część paska.</p>
+      <div class="table-container">
+        <table>
+          <tr>
+            <td>
+              <strong>Kolor:</strong>
+            </td>
+            <td>czarny, niebieski, zielony</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Wymiary:</strong>
+            </td>
+            <td>265 x 36 x 13 mm</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Materiał:</strong>
+            </td>
+            <td>Tworzywo</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Opakowanie produktu:</strong>
+            </td>
+            <td>Opakowanie</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Przykładowa cecha:</strong>
+            </td>
+            <td>Przykład</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <div class="tab-pane fade" id="nav-opinions" role="tabpanel" aria-labelledby="nav-opinions-tab" tabindex="0">
+      Miejsce na widget
+    </div>
+  </div>
 </div>
