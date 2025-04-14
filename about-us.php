b5ea8df257b4e67cec797a06cab8fff51723ea31
goldipl <mgodlewski92@gmail.com>
2025-04-14 13:26:51 +0200

    Add template and styles for About Us subpage

---
+++ about-us.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PRIMA GIFT - O nas</title>
+        <link rel="shortcut icon" href="./assets/icons/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="./css/bootstrap.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/swiper-bundle.css" />
+        <link rel="stylesheet" href="./css/select2.min.css" />
+        <link rel="stylesheet" href="./scss/main.css">
+    </head>
+    <body>
+        <header> 
+            <?php include "./components/common/nav.php"; ?> 
+        </header>
+        <main id="main-wrapper">
+            <div class="breadcrumbs container">
+                <ul>
+                    <li class="breadcrumb-item">
+                        <a href="./index.php">Strona główna</a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <span>O nas</span>
+                    </li>
+                </ul>
+            </div>
+            <?php include "./components/about_us/about_us_content.php"; ?> 
+            <?php include "./components/main_page/our_brands.php"; ?>
+        </main>
+        <footer> 
+            <?php include "./components/common/footer.php"; ?> 
+        </footer>
+        <script src="./js/jquery.min.js"></script>
+        <script src="./js/popper.min.js"></script>
+        <script src="./js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="./js/bootbox.min.js"></script>
+        <script src="./js/select2.min.js"></script>
+        <script src="./js/swiper/swiper-bundle.js"></script>
+        <script src="./js/swiper/swiper.js"></script>
+        <script src="./js/script.js"></script>
+    </body>
+</html>
---
+++ components/about_us/about_us_content.php
@@ -0,0 +1,88 @@
+<div class="hello-box container">
+    <div class="hello-box-wrapper">
+        <div class="hello-box-wrapper__text">
+            <h1>Witaj w <span>Prima Gift!</span></h1>
+            <h3><strong>Miło cię gościć na naszej stronie.</strong></h3>
+            <strong>Czym się zajmujemy?</strong>
+            <p>Specjalizujemy się w realizacji gadżetów reklamowych oraz odzieży ze znakowaniem. Zapewniamy kompleksową obsługę oraz wysoką jakość oferowanych produktów. Tworzymy innowacyjne i unikalne rozwiązania dostosowane do Twoich potrzeb.</p>
+            <p>Wiemy jak ważny jest dla Ciebie wizerunek Twojej firmy, dlatego naszym celem jest wsparcie w jego kreowaniu. Zadbamy o Twoje potrzeby oraz pomożemy ci się wyróżnić.</p>
+        </div>
+        <div class="hello-box-wrapper__image">
+            <img src="./assets/img/about_us/hello.png" alt="Hello" width="171" height="160" />
+        </div>
+    </div>
+</div>
+<div class="img-text-boxes container">
+    <div class="img-text-boxes-wrapper">
+        <div class="img-text-boxes-wrapper__left">
+            <div class="big-txt-img-box">
+                <div class="big-txt-img-box__text">
+                    <span>Dlaczego my?</span>
+                    <span>To proste:</span>
+                </div>
+                <div class="big-txt-img-box__img">
+                    <img src="./assets/img/about_us/present.png" alt="Prezent Box" width="167" height="157" />
+                </div>
+            </div>
+        </div>
+        <div class="img-text-boxes-wrapper__right">
+            <div class="small-txt-img-box">
+                <div class="small-txt-img-box__img">
+                    <img src="./assets/icons/about_us/happy-clients.svg" alt="setki zadowolonych klientów" width="70" height="70" />
+                </div>
+                <div class="small-txt-img-box__text">
+                    <span>setki zadowolonych klientów</span>
+                </div>
+            </div>
+            <div class="small-txt-img-box">
+                <div class="small-txt-img-box__img">
+                    <img src="./assets/icons/about_us/boxes.svg" alt="setki zadowolonych klientów" width="70" height="70" />
+                </div>
+                <div class="small-txt-img-box__text">
+                    <span>setki zadowolonych klientów</span>
+                </div>
+            </div>
+            <div class="small-txt-img-box">
+                <div class="small-txt-img-box__img">
+                    <img src="./assets/icons/about_us/fast-time.svg" alt="szybki czas realizacji" width="70" height="70" />
+                </div>
+                <div class="small-txt-img-box__text">
+                    <span>szybki czas realizacji</span>
+                </div>
+                <div class="small-txt-img-box__pink-lines">
+                    <img src="./assets/icons/common/pink-lines.svg" alt="różowe linie" width="66" height="50" />
+                </div>
+            </div>
+            <div class="small-txt-img-box">
+                <div class="small-txt-img-box__img">
+                    <img src="./assets/icons/about_us/individual.svg" alt="indywidualne podejście" width="70" height="70" />
+                </div>
+                <div class="small-txt-img-box__text">
+                    <span>indywidualne podejście</span>
+                </div>
+                <div class="small-txt-img-box__pink-lines">
+                    <img src="./assets/icons/common/pink-lines.svg" alt="różowe linie" width="66" height="50" />
+                </div>
+            </div>
+            <div class="small-txt-img-box">
+                <div class="small-txt-img-box__img">
+                    <img src="./assets/icons/about_us/help-on-every-step.svg" alt="pomoc na każdym etapie realizacji" width="70" height="70" />
+                </div>
+                <div class="small-txt-img-box__text">
+                    <span>pomoc na każdym etapie realizacji</span>
+                </div>
+            </div>
+            <div class="small-txt-img-box">
+                <div class="small-txt-img-box__img">
+                    <img src="./assets/icons/about_us/const-help.svg" alt="stały kontakt i doradztwo" width="70" height="70" />
+                </div>
+                <div class="small-txt-img-box__text">
+                    <span>stały kontakt i doradztwo</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="button-wrapper">
+        <a href="" class="img-text-boxes-btn">sprawdź ofertę</a>
+    </div>
+</div>
