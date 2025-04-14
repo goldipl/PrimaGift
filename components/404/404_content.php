66f3bc4a5eaf14179b00334c836a68f77e266dc5
goldipl <mgodlewski92@gmail.com>
2025-04-14 18:16:14 +0200

    Add 404 error page and content components for improved user experience

---
+++ components/404/404_content.php
@@ -0,0 +1,10 @@
+<div class="wrapper-404">
+    <div class="wrapper-404__img">
+        <img src="./assets/img/404/404.png" alt="Błąd 404" class="img-404" width="844" height="574">
+    </div>
+    <div class="wrapper-404__text">
+        <h2>UPS! Coś poszło nie tak...</h2>
+        <span>Strona, której szukasz nie istnieje lub link jest niepoprawny.</span>
+        <a href="" class="color-btn">wróć na stronę główną</a>
+    </div>
+</div>
