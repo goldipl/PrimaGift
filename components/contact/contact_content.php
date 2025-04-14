8a4b1411a1c54a609a743f3d5c413ba35a881fb6
goldipl <mgodlewski92@gmail.com>
2025-04-14 20:41:43 +0200

    fill & styling contact page

--- components/contact/contact_content.php
+++ components/contact/contact_content.php
@@ -5,27 +5,37 @@
       <h3>Jesteśmy tu dla Ciebie! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi condimentum turpis eu ligula dapibus euismod. Nam ut ex a nulla pellentesque interdum id sodales nisi.</h3>
       <div class="contact-grey-box">
         <div class="grey-left-col">
-          <a class="gtm-phone" href="tel:+48 032 750 86 56">
-            <img src="./assets/icons/contact/pink-location.svg" width="22" height="22" alt="telefon">+48 032 750 86 56</a>
-          <a class="gtm-phone" href="tel:+48 032 750 86 56">
-            <img src="./assets/icons/contact/pink-hours.svg" width="22" height="22" alt="telefon">+48 032 750 86 56</a>
+          <div class="location-address">
+            <img src="./assets/icons/contact/pink-location.svg" width="22" height="22" alt="adres">
+            <div class="address-col">
+              <strong>Prima Gift</strong>
+              <span>ul. Józefa Pukowca 15,</span>
+              <span>40-847 Katowice</span>
+            </div>
+          </div>
+          <div class="hours">
+            <img src="./assets/icons/contact/pink-hours.svg" width="22" height="22" alt="godziny">
+            <strong>pn-pt.: 8.00 - 16.00</strong>
+          </div>
         </div>
         <div class="grey-right-col">
           <a class="gtm-phone" href="tel:+48 032 750 86 56">
-            <img src="./assets/icons/contact/pink-cellphone.svg" width="22" height="22" alt="telefon">+48 032 750 86 56</a>
-          <a class="gtm-mail" href="tel:+48 032 750 86 56">
-            <img src="./assets/icons/contact/pink-letter.svg" width="22" height="22" alt="telefon">+48 032 750 86 56</a>
+            <img src="./assets/icons/contact/pink-cellphone.svg" width="22" height="22" alt="telefon">+48 032 750 86 56 </a>
+          <a class="gtm-mail" href="tmailto:biuro@primagift.pl">
+            <img src="./assets/icons/contact/pink-letter.svg" width="22" height="22" alt="e-mail">biuro@primagift.pl </a>
         </div>
       </div>
       <div class="contact-map">
         <h2>
-          <img src="./assets/img/contact/location.jpg" width="80" height="80" alt="mapa" />Tutaj nas znajdziesz
+          <img src="./assets/img/contact/location.jpg" width="80" height="80" alt="mapa" />Tutaj nas znajdziesz!
         </h2>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2550.5718203483593!2d18.98850507768904!3d50.26258087155702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716ce6d5f36c469%3A0x4baf0944ea7146a2!2sJ%C3%B3zefa%20Pukowca%2015%2C%2040-847%20Katowice!5e0!3m2!1spl!2spl!4v1744647780052!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
     <div class="contact-wrapper__col">
       <form class="send-form">
+        <h2>Masz pytania?</h2>
+        <h3>Potrzebujesz wyceny lub innej informacji? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi condimentum turpis eu ligula dapibus euismod.</h3>
         <div class="input-slot">
           <label for="name-surname">Imię i nazwisko: *</label>
           <input class="form-input" type="text" name="name-surname" id="name-surname" placeholder="Anna Kowalska" required>
@@ -42,6 +52,10 @@
           <label for="msgarea">Treść wiadomości:</label>
           <textarea class="form-textarea" name="msgarea" id="msgarea" placeholder="W czy możemy Ci pomóc?"></textarea>
         </div>
+        <label class="custom-checkbox-container">
+          <div class="register-checkbox"></div>Wyrażam zgodę na przetwarzanie danych osobowych wpisanych w formularzu w celu otrzymania odpowiedzi na moje pytanie zgodnie z Polityką Prywatności. <input type="checkbox" class="custom_input">
+          <span class="custom-checkbox-checkmark"></span>
+        </label>
         <div class="checkbox-button-box">
           <button class="color-btn">wyślij wiadomość</button>
         </div>
