110a67e5e2d574a8dd3b25a9886631d95dc29b95
goldipl <mgodlewski92@gmail.com>
2025-04-14 18:15:55 +0200

    Add similar products section with swiper functionality

---
+++ components/404/similar_products.php
@@ -0,0 +1,257 @@
+<section id="our_products_swiper" class="container position-relative similar-products-swiper">
+  <h2 class="d-flex justify-content-center">Może zainteresują Cię...</h2>
+  <div class="similar_products_swiper">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+      <!-- Slide -->
+      <div class="swiper-slide">
+        <div class="swiper_products_block_box">
+          <div class="swiper_products_block_box__top">
+            <a href="#" rel="nofollow" class="products-image">
+              <img src="./assets/img/main_page/products/thermophor.png" alt="products block img" width="200" height="200">
+            </a>
+            <div class="products-title">
+              <h3 class="text-center">
+                <a href="#">Termofor LIDO</a>
+              </h3>
+            </div>
+            <div class="catalog-price">
+              <p class="text-center">
+                <span>29,50 zł netto</span>
+              </p>
+            </div>
+          </div>
+          <div class="swiper_products_block_box__bottom">
+            <div class="color-squares">
+              <a href="" class="color-square" style="background: #000;"></a>
+              <a href="" class="color-square" style="background: #ffffff; border: 1px solid #D3CFCC;"></a>
+              <a href="" class="color-square" style="background: #D3CFCC;"></a>
+            </div>
+            <a href="" class="products-buy-button">
+              <img src="./assets/icons/common/add-to-shopping-list-icon.svg" alt="Dodaj" width="26" height="26">
+            </a>
+          </div>
+          <!-- Product label -->
+          <div class="product-label-box">
+            <div class="product-label" style="background-color: #ED6A8A;">
+              <p>Bestseller</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Slide -->
+      <div class="swiper-slide">
+        <div class="swiper_products_block_box">
+          <div class="swiper_products_block_box__top">
+            <a href="#" rel="nofollow" class="products-image">
+              <img src="./assets/img/main_page/products/bottle.png" alt="products block img" width="200" height="200">
+            </a>
+            <div class="products-title">
+              <h3 class="text-center">
+                <a href="#">Butelka termiczna TILLI 500 ml</a>
+              </h3>
+            </div>
+            <div class="catalog-price">
+              <p class="text-center">
+                <span>49,50 zł netto</span>
+              </p>
+            </div>
+          </div>
+          <div class="swiper_products_block_box__bottom">
+            <div class="color-squares">
+              <a href="" class="color-square" style="background: #000;"></a>
+              <a href="" class="color-square" style="background: #ffffff; border: 1px solid #D3CFCC;"></a>
+              <a href="" class="color-square" style="background: #D3CFCC;"></a>
+              <a href="" class="color-square" style="background: #0000A0;"></a>
+            </div>
+            <a href="" class="products-buy-button">
+              <img src="./assets/icons/common/add-to-shopping-list-icon.svg" alt="Dodaj" width="26" height="26">
+            </a>
+          </div>
+          <!-- Product label -->
+          <div class="product-label-box">
+            <div class="product-label" style="background-color: #ED6A8A;">
+              <p>Bestseller</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Slide -->
+      <div class="swiper-slide">
+        <div class="swiper_products_block_box">
+          <div class="swiper_products_block_box__top">
+            <a href="#" rel="nofollow" class="products-image">
+              <img src="./assets/img/main_page/products/tea-pot.png" alt="products block img" width="200" height="200">
+            </a>
+            <div class="products-title">
+              <h3 class="text-center">
+                <a href="#">Dzbanek szklany BOLLITO 1100 ml</a>
+              </h3>
+            </div>
+            <div class="catalog-price">
+              <p class="text-center">
+                <span>49,90 zł netto</span>
+              </p>
+            </div>
+          </div>
+          <div class="swiper_products_block_box__bottom">
+            <div class="color-squares"></div>
+            <a href="" class="products-buy-button">
+              <img src="./assets/icons/common/add-to-shopping-list-icon.svg" alt="Dodaj" width="26" height="26">
+            </a>
+          </div>
+          <!-- Product label -->
+          <div class="product-label-box">
+            <div class="product-label" style="background-color: #ED6A8A;">
+              <p>Bestseller</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Slide -->
+      <div class="swiper-slide">
+        <div class="swiper_products_block_box">
+          <div class="swiper_products_block_box__top">
+            <a href="#" rel="nofollow" class="products-image">
+              <img src="./assets/img/main_page/products/powerbank.png" alt="products block img" width="200" height="200">
+            </a>
+            <div class="products-title">
+              <h3 class="text-center">
+                <a href="#">Bambusowy power bank 8000 mAh, ładowarka słoneczna</a>
+              </h3>
+            </div>
+            <div class="catalog-price">
+              <p class="text-center">
+                <span>75,50 zł netto</span>
+              </p>
+            </div>
+          </div>
+          <div class="swiper_products_block_box__bottom">
+            <div class="color-squares">
+              <a href="" class="color-square" style="background: #000;"></a>
+              <a href="" class="color-square" style="background: #ffffff; border: 1px solid #D3CFCC;"></a>
+              <a href="" class="color-square" style="background: #D3CFCC;"></a>
+            </div>
+            <a href="" class="products-buy-button">
+              <img src="./assets/icons/common/add-to-shopping-list-icon.svg" alt="Dodaj" width="26" height="26">
+            </a>
+          </div>
+          <!-- Product label -->
+          <div class="product-label-box">
+            <div class="product-label" style="background-color: #ED6A8A;">
+              <p>Bestseller</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Slide -->
+      <div class="swiper-slide">
+        <div class="swiper_products_block_box">
+          <div class="swiper_products_block_box__top">
+            <a href="#" rel="nofollow" class="products-image">
+              <img src="./assets/img/main_page/products/bottle.png" alt="products block img" width="200" height="200">
+            </a>
+            <div class="products-title">
+              <h3 class="text-center">
+                <a href="#">Butelka termiczna TILLI 500 ml</a>
+              </h3>
+            </div>
+            <div class="catalog-price">
+              <p class="text-center">
+                <span>49,50 zł netto</span>
+              </p>
+            </div>
+          </div>
+          <div class="swiper_products_block_box__bottom">
+            <div class="color-squares">
+              <a href="" class="color-square" style="background: #000;"></a>
+              <a href="" class="color-square" style="background: #ffffff; border: 1px solid #D3CFCC;"></a>
+              <a href="" class="color-square" style="background: #D3CFCC;"></a>
+              <a href="" class="color-square" style="background: #0000A0;"></a>
+            </div>
+            <a href="" class="products-buy-button">
+              <img src="./assets/icons/common/add-to-shopping-list-icon.svg" alt="Dodaj" width="26" height="26">
+            </a>
+          </div>
+          <!-- Product label -->
+          <div class="product-label-box">
+            <div class="product-label" style="background-color: #ED6A8A;">
+              <p>Bestseller</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Slide -->
+      <div class="swiper-slide">
+        <div class="swiper_products_block_box">
+          <div class="swiper_products_block_box__top">
+            <a href="#" rel="nofollow" class="products-image">
+              <img src="./assets/img/main_page/products/tea-pot.png" alt="products block img" width="200" height="200">
+            </a>
+            <div class="products-title">
+              <h3 class="text-center">
+                <a href="#">Dzbanek szklany BOLLITO 1100 ml</a>
+              </h3>
+            </div>
+            <div class="catalog-price">
+              <p class="text-center">
+                <span>49,90 zł netto</span>
+              </p>
+            </div>
+          </div>
+          <div class="swiper_products_block_box__bottom">
+            <div class="color-squares"></div>
+            <a href="" class="products-buy-button">
+              <img src="./assets/icons/common/add-to-shopping-list-icon.svg" alt="Dodaj" width="26" height="26">
+            </a>
+          </div>
+          <!-- Product label -->
+          <div class="product-label-box">
+            <div class="product-label" style="background-color: #ED6A8A;">
+              <p>Bestseller</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Slide -->
+      <div class="swiper-slide">
+        <div class="swiper_products_block_box">
+          <div class="swiper_products_block_box__top">
+            <a href="#" rel="nofollow" class="products-image">
+              <img src="./assets/img/main_page/products/powerbank.png" alt="products block img" width="200" height="200">
+            </a>
+            <div class="products-title">
+              <h3 class="text-center">
+                <a href="#">Bambusowy power bank 8000 mAh, ładowarka słoneczna</a>
+              </h3>
+            </div>
+            <div class="catalog-price">
+              <p class="text-center">
+                <span>75,50 zł netto</span>
+              </p>
+            </div>
+          </div>
+          <div class="swiper_products_block_box__bottom">
+            <div class="color-squares">
+              <a href="" class="color-square" style="background: #000;"></a>
+              <a href="" class="color-square" style="background: #ffffff; border: 1px solid #D3CFCC;"></a>
+              <a href="" class="color-square" style="background: #D3CFCC;"></a>
+            </div>
+            <a href="" class="products-buy-button">
+              <img src="./assets/icons/common/add-to-shopping-list-icon.svg" alt="Dodaj" width="26" height="26">
+            </a>
+          </div>
+          <!-- Product label -->
+          <div class="product-label-box">
+            <div class="product-label" style="background-color: #ED6A8A;">
+              <p>Bestseller</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- If we need navigation buttons -->
+    <div class="swiper-similar-products-button-prev"></div>
+    <div class="swiper-similar-products-button-next"></div>
+  </div>
+</section>
