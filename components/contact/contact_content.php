5c184c7768b37bcb25fa5b963adc42cbee718c35
goldipl <mgodlewski92@gmail.com>
2025-04-14 20:03:06 +0200

    Refactor contact page: update structure, improve content clarity, and enhance form fields for better user experience

--- components/contact/contact_content.php
+++ components/contact/contact_content.php
@@ -1,58 +1,51 @@
 <div class="contact-container container">
-    <h4>Skontaktuj się z znami</h4>
-    <h1>Możesz zadawać nam pytania </h1>
-    <h4>Skontaktuj się z nami, aby uzyskać odpowiedzi na wszystkie pytania i opinie, lub możesz rozwiązać swoje problemy w krótkim czasie dzięki naszemu zespołowi.</h4>
-    <div class="contact-wrapper">
-        <div class="contact-wrapper_col">
-            <div class="contact-grey-box">
-                <div class="grey-left-col">
-                    <h3>Skontaktuj się</h3>
-                    <a class="gtm-phone" href="tel:+48 032 750 86 56"><img src="./assets/icons/contact/telephone.svg" width="22" height="22" alt="telefon">+48 032 750 86 56</a>
-                    <a class="gtm-phone" href="tel:+48 071 722 08 26"><img src="./assets/icons/contact/telephone.svg" width="22" height="22" alt="telefon">+48 071 722 08 26</a>
-                    <a class="gtm-phone" href="tel:+48 505 178 262"><img src="./assets/icons/contact/smartphone.svg" width="22" height="22" alt="telefon">+48 505 178 262</a>
-                    <a class="gtm-mail" href="mailto: biuro@PRIMA GIFT.pl"><img src="./assets/icons/contact/letter.svg" width="22" height="22" alt="email"> biuro@PRIMA GIFT.pl</a>
-                </div>
-                <div class="grey-right-col">
-                    <h3>Adres</h3>
-                    <p>Biznes Promotion</p>
-                    <p>ul. Trzebnicka 26</p>
-                    <p>51-217 Pruszowice</p>
-                    <p>Polska</p>
-                </div>
-            </div>
+  <div class="contact-wrapper">
+    <div class="contact-wrapper__col">
+      <h1>Skontaktuj się z nami</h1>
+      <h3>Jesteśmy tu dla Ciebie! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi condimentum turpis eu ligula dapibus euismod. Nam ut ex a nulla pellentesque interdum id sodales nisi.</h3>
+      <div class="contact-grey-box">
+        <div class="grey-left-col">
+          <a class="gtm-phone" href="tel:+48 032 750 86 56">
+            <img src="./assets/icons/contact/pink-location.svg" width="22" height="22" alt="telefon">+48 032 750 86 56</a>
+          <a class="gtm-phone" href="tel:+48 032 750 86 56">
+            <img src="./assets/icons/contact/pink-hours.svg" width="22" height="22" alt="telefon">+48 032 750 86 56</a>
         </div>
-        <div class="contact-wrapper_col">
-            <div class="contact-wrapper_col__content">
-                <form class="send-form">
-                    <div class="send-form-left">
-                        <div class="input-slot">
-                            <label for="name-surname">Imię *</label>
-                            <input class="form-input" type="text" name="name-surname" id="name-surname" placeholder="np. Jan" required>
-                        </div>
-                        <div class="input-slot">
-                            <label for="email">E-mail *</label>
-                            <input class="form-input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" id="email" placeholder="np. multimaszyny@gmail.com">
-                        </div>
-                        <div class="input-slot">
-                            <label for="telephone">Telefon</label>
-                            <input class="form-input" type="text" name="telephone" id="telephone" placeholder="np. 516696767">
-                        </div>
-                        <img src="./assets/icons/contact/captcha.svg" width="259" height="50" alt="captcha">
-                    </div>
-                    <div class="send-form-right">
-                        <div class="input-slot">
-                            <label for="msgarea">Wiadomość</label>
-                            <textarea class="form-textarea" name="msgarea" id="msgarea" placeholder="Tutaj możesz wpisać swoją wiadomość"></textarea>
-                        </div>
-                        <div class="checkbox-button-box">
-                            <button class="color-btn">wyślij wiadomość</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+        <div class="grey-right-col">
+          <a class="gtm-phone" href="tel:+48 032 750 86 56">
+            <img src="./assets/icons/contact/pink-cellphone.svg" width="22" height="22" alt="telefon">+48 032 750 86 56</a>
+          <a class="gtm-mail" href="tel:+48 032 750 86 56">
+            <img src="./assets/icons/contact/pink-letter.svg" width="22" height="22" alt="telefon">+48 032 750 86 56</a>
         </div>
-    </div>
-    <div class="contact-map">
+      </div>
+      <div class="contact-map">
+        <h2>
+          <img src="./assets/img/contact/location.jpg" width="80" height="80" alt="mapa" />Tutaj nas znajdziesz
+        </h2>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2550.5718203483593!2d18.98850507768904!3d50.26258087155702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716ce6d5f36c469%3A0x4baf0944ea7146a2!2sJ%C3%B3zefa%20Pukowca%2015%2C%2040-847%20Katowice!5e0!3m2!1spl!2spl!4v1744647780052!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
     </div>
+    <div class="contact-wrapper__col">
+      <form class="send-form">
+        <div class="input-slot">
+          <label for="name-surname">Imię i nazwisko: *</label>
+          <input class="form-input" type="text" name="name-surname" id="name-surname" placeholder="Anna Kowalska" required>
+        </div>
+        <div class="input-slot">
+          <label for="email">Adres e-mail: *</label>
+          <input class="form-input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" id="email" placeholder="przykladowy@mail.com">
+        </div>
+        <div class="input-slot">
+          <label for="telephone">Numer telefonu:</label>
+          <input class="form-input" type="text" name="telephone" id="telephone" placeholder="np. +48 000 999 000">
+        </div>
+        <div class="input-slot">
+          <label for="msgarea">Treść wiadomości:</label>
+          <textarea class="form-textarea" name="msgarea" id="msgarea" placeholder="W czy możemy Ci pomóc?"></textarea>
+        </div>
+        <div class="checkbox-button-box">
+          <button class="color-btn">wyślij wiadomość</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
