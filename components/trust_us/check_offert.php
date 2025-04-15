64060ca1072432f19b0989dce502855ba133cc70
goldipl <mgodlewski92@gmail.com>
2025-04-15 15:56:34 +0200

    Add Trust Us section with styling and content
    - Introduced new SCSS files for the Trust Us section, including styles for check-offert and trust_us_content.
    - Updated main.scss to import the new Trust Us styles.
    - Created trust-us.php to serve as the template for the Trust Us page, including breadcrumbs and component inclusions for content and offers.

---
+++ components/trust_us/check_offert.php
@@ -0,0 +1,19 @@
+<div class="check-offert">
+    <div class="check-offert-box container">
+        <div class="check-offert-top-shape">
+            <img src="./assets/icons/brands/top-black-vector.svg" alt="czarny kształt" class="check-offert-top-shape--img" width="1440" height="30">
+        </div>
+        <div class="check-offert-box-content">
+            <div class="check-offert-box-content__img">
+                <img src="./assets/icons/brands/happy-clients.svg" alt="I Ty dołącz do grona naszych zadowolonych klientów!" width="98" height="104">
+            </div>
+            <div class="check-offert-box-content__text">
+                <strong>I Ty dołącz do grona naszych zadowolonych klientów!</strong>
+                <a href="" class="color-btn">sprawdź ofertę</a>
+            </div>
+        </div>
+        <div class="check-offert-bottom-shape">
+            <img src="./assets/icons/brands/bottom-pink-vector.svg" alt="różowy kształt" class="check-offert-bottom-shape--img" width="1408" height="30">
+        </div>
+    </div>
+</div>
---
+++ components/trust_us/trust_us_content.php
@@ -0,0 +1,106 @@
+<div class="trusted-clients">
+    <h1>Klienci, którzy nam zaufali</h1>
+    <h3>Dziękujemy!</h3>
+    <div class="white-container">
+        <div class="trusted-clients-box container">
+            <div class="trusted-clients-box__row">
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/mercator.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/bn-solutions.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/langowski.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/unibep.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/pzz-walcz.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+            </div>
+            <div class="trusted-clients-box__row">
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/lukow.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/lavazza.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/makurat.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/ppm-t.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/grupa-econ.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+            </div>
+            <div class="trusted-clients-box__row">
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/magnaplast.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/polski-gaz.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/akademia-wsb.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/saunier-duval.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/vaillant.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+            </div>
+            <div class="trusted-clients-box__row">
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/kss-bartnica.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/minsk-mazowiecki.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+                <div class="trusted-clients-box__slot">
+                    <div class="trusted-clients__img">
+                        <img src="./assets/img/brands/veltech.png" alt="Firma" width="100" height="100">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
