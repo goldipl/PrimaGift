084bd089f42d1291e9941e53fa6771cb3e44e54e
goldipl <mgodlewski92@gmail.com>
2025-04-15 14:28:17 +0200

    add branding techniques and how to buy components

---
+++ components/branding_techniques/branding_techniques.php
@@ -0,0 +1,97 @@
+<div class="branding-techniques">
+    <div class="container">
+        <h1>Techniki znakowania</h1>
+        <div class="branding-techniques-box">
+            <div class="branding-techniques-box-slot">
+                <div class="branding-techniques-box-slot__img">
+                    <img src="./assets/img/branding_techniques/tampodruk.png" alt="Tampodruk" width="512" height="324" loading="lazy">
+                </div>
+                <div class="branding-techniques-box-slot__text">
+                    <h2>Tampodruk</h2>
+                    <p>Jedna z najbardziej popularnych oraz wszechstronnych metod znakowania. Polega na nanoszeniu farby za pomocą miękkiego stempla zwanego tamponem na powierzchnie znakowaną. Produkty najczęściej znakowane tampodrukiem: plastikowe długopisy, antystresy, breloki, odblaski.</p>
+                </div>
+            </div>
+            <div class="branding-techniques-box-slot">
+                <div class="branding-techniques-box-slot__img">
+                    <img src="./assets/img/branding_techniques/grawer-laserowy.png" alt="Grawer laserowy" width="512" height="324" loading="lazy">
+                </div>
+                <div class="branding-techniques-box-slot__text">
+                    <h2>Grawer laserowy</h2>
+                    <p>Polega na wypaleniu zewnętrznej warstwy produktu. Najczęściej jest stosowany na przedmiotach metalowych. W przypadku graweru nie mamy wpływu na kolor znakowania – wychodzi on z podłoża.</p>
+                </div>
+            </div>
+            <div class="branding-techniques-box-slot">
+                <div class="branding-techniques-box-slot__img">
+                    <img src="./assets/img/branding_techniques/sitodruk.png" alt="Sitodruk" width="512" height="324" loading="lazy">
+                </div>
+                <div class="branding-techniques-box-slot__text">
+                    <h2>Sitodruk</h2>
+                    <p>Technika druku bezpośredniego na tekstyliach. Farba przy pomocy rakli przeciskana jest z wcześniej przygotowanych matryc (sit). Świetnie sprawdza się przy druku o dużej powierzchni.</p>
+                </div>
+            </div>
+            <div class="branding-techniques-box-slot">
+                <div class="branding-techniques-box-slot__img">
+                    <img src="./assets/img/branding_techniques/haft-komputerowy.png" alt="Haft komputerowy" width="512" height="324" loading="lazy">
+                </div>
+                <div class="branding-techniques-box-slot__text">
+                    <h2>Haft komputerowy</h2>
+                    <p>Haft to wyszywanie przez maszynę określonego wzoru na materiale. Bardzo trwała i elegancka technika, stosowana na wszelkich materiałach tekstylnych.</p>
+                </div>
+            </div>
+            <div class="branding-techniques-box-slot">
+                <div class="branding-techniques-box-slot__img">
+                    <img src="./assets/img/branding_techniques/druk-cyfrowy.png" alt="Druk cyfrowy" width="512" height="324" loading="lazy">
+                </div>
+                <div class="branding-techniques-box-slot__text">
+                    <h2>Druk cyfrowy</h2>
+                    <p>Nowoczesna i szybka metoda znakowania w palecie CMYK. Pozwala na osiągnięcie fotograficznej jakości wydruku. Stosowana na płaskich powierzchniach.</p>
+                </div>
+            </div>
+            <div class="branding-techniques-box-slot">
+                <div class="branding-techniques-box-slot__img">
+                    <img src="./assets/img/branding_techniques/tloczenie.png" alt="Tłoczenie" width="512" height="324" loading="lazy">
+                </div>
+                <div class="branding-techniques-box-slot__text">
+                    <h2>Tłoczenie</h2>
+                    <p>Polega na odciśnięciu matrycy w materiale pod wpływem dużego ciśnienia oraz wysokiej temperatury. Wykorzystywane jest przy znakowaniu produktów ze skóry lub skóropodobnych (notesy, portfele, kosmetyczki).</p>
+                </div>
+            </div>
+            <div class="branding-techniques-box-slot">
+                <div class="branding-techniques-box-slot__img">
+                    <img src="./assets/img/branding_techniques/transfer.png" alt="Transfer" width="512" height="324" loading="lazy">
+                </div>
+                <div class="branding-techniques-box-slot__text">
+                    <h2>Transfer</h2>
+                    <p>Technika polegająca na wgrzaniu przygotowanego na specjalnym papierze wydruku w dany materiał. Wykorzystywana jest głównie do znakowania materiałów tekstylnych przy niskich nakładach.</p>
+                </div>
+            </div>
+            <div class="branding-techniques-box-slot">
+                <div class="branding-techniques-box-slot__img">
+                    <img src="./assets/img/branding_techniques/kalka-ceramiczna.png" alt="Kalka ceramiczna" width="512" height="324" loading="lazy">
+                </div>
+                <div class="branding-techniques-box-slot__text">
+                    <h2>Kalka ceramiczna</h2>
+                    <p>Technika polega na nasieniu, a następnie wypaleniu nadruku na produkcie ceramicznym. Kalka wypalana jest w temperaturze 600 stopni celsjusza co gwarantuje jej odporność na zmywanie w zmywarkach.</p>
+                </div>
+            </div>
+            <div class="branding-techniques-box-slot">
+                <div class="branding-techniques-box-slot__img">
+                    <img src="./assets/img/branding_techniques/doming.png" alt="Doming" width="512" height="324" loading="lazy">
+                </div>
+                <div class="branding-techniques-box-slot__text">
+                    <h2>Doming</h2>
+                    <p>Wypukła żywiczna naklejka pozwalająca uzyskać efekt zbliżony do trójwymiarowego obrazu. Cechuje ją wysoka jakość oraz wyraziste kolory. Stosowana na płaskich powierzchniach np. na brelokach.</p>
+                </div>
+            </div>
+            <div class="branding-techniques-box-slot">
+                <div class="branding-techniques-box-slot__img">
+                    <img src="./assets/img/branding_techniques/DTF.png" alt="DTF" width="512" height="324" loading="lazy">
+                </div>
+                <div class="branding-techniques-box-slot__text">
+                    <h2>DTF</h2>
+                    <p>Najnowsza z technik znakowania polegająca na przeniesieniu druku cyfrowego z folii na produkt. Folia pokrywana jest specjalnym proszkiem, który jest aktywowany w tunelu grzewczym. Stosowana na materiałach tekstylnych.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ components/branding_techniques/check_how_to_buy.php
@@ -0,0 +1,12 @@
+<div class="check-how-to-buy">
+    <div class="check-how-to-buy-box container">
+        <div class="check-how-to-buy-box__img">
+            <img src="./assets/img/branding_techniques/sprawdz-jak-kupowac.png" alt="Sprawdź jak kupować" width="140" height="140" class="check-how-to-buy-box__img--img">
+        </div>
+        <div class="check-how-to-buy-box__text">
+            <h3>Sprawdź jak kupować</h3>
+            <span>Zamów gadżety w szybki i prosty sposób! Przeczytaj naszą instrukcję krok po kroku!</span>
+            <a href="" class="color-btn">dowiedz się więcej</a>
+        </div>
+    </div>
+</div>
