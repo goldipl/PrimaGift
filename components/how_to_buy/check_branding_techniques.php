7a2670929080e4c941f9c48195b0662364e9f8cb
goldipl <mgodlewski92@gmail.com>
2025-04-24 17:18:56 +0200

    feat: add branding techniques image and update text in bottom teaser

--- components/how_to_buy/check_branding_techniques.php
+++ components/how_to_buy/check_branding_techniques.php
@@ -1,11 +1,11 @@
 <div class="bottom-teaser">
     <div class="bottom-teaser-box container">
         <div class="bottom-teaser-box__img">
-            <img src="./assets/img/branding_techniques/sprawdz-jak-kupowac.png" alt="Sprawdź jak kupować" width="140" height="140" class="bottom-teaser-box__img--img">
+            <img src="./assets/img/how_to_buy/branding-techniques.jpg" alt="Sprawdź techniki znakowania" width="140" height="140" class="bottom-teaser-box__img--img">
         </div>
         <div class="bottom-teaser-box__text">
-            <h3>Sprawdź jak kupować</h3>
-            <span>Zamów gadżety w szybki i prosty sposób! Przeczytaj naszą instrukcję krok po kroku!</span>
+            <h3>Sprawdź techniki znakowania</h3>
+            <span>Oferujemy wszelkie możliwe techniki znakowania dostępne na rynku. </span>
             <a href="" class="color-btn">dowiedz się więcej</a>
         </div>
     </div>
