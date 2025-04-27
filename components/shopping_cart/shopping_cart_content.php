0b51568fbf0f81cffa6f8ef7510cd6318b440fa0
goldipl <mgodlewski92@gmail.com>
2025-04-27 15:30:14 +0200

    Add styles for wyslij-zapytanie section and enhance shopping cart headings
    - Updated h1 styles in the shopping cart to include h2 with consistent font properties.
    - Introduced new styles for the wyslij-zapytanie section, including layout for form inputs and responsive design adjustments.
    - Added flexbox layout for input rows and slots to improve form usability on different screen sizes.
    - Ensured proper styling for textarea and buttons within the new section.

--- components/shopping_cart/shopping_cart_content.php
+++ components/shopping_cart/shopping_cart_content.php
@@ -78,6 +78,78 @@
                 <td>
                   <input type="number" min="0" name="product-quantity" id="product-quantity">
                 </td>
+                <td>120,95 zł</td>
+                <td>120,09 PLN</td>
+                <td>120,34 PLN</td>
+                <td>
+                  <a class="delete-icon" href="#">
+                    <img src="./assets/icons/shopping_cart/delete.svg" alt="delete" width="17" height="20">
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td class="table-number-col">
+                  <b>3</b>
+                </td>
+                <td class="table-product-col">
+                  <div class="table-product-box">
+                    <div class="table-product-box__top">
+                      <p>Produkt 3</p>
+                    </div>
+                    <div class="table-product-box__bottom">
+                      <div class="table-product-box__bottom--left">
+                        <img src="./assets/img/products_card/smartwatch.jpg" alt="img" width="71" height="71">
+                      </div>
+                      <div class="table-product-box__bottom--right">
+                        <p>Kod: <span>1333903</span>
+                        </p>
+                        <p>Kolor: <span>czarny</span>
+                        </p>
+                        <p>Marka: <span>Basic</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <input type="number" min="0" name="product-quantity" id="product-quantity">
+                </td>
+                <td>100,95 zł</td>
+                <td>100,09 PLN</td>
+                <td>100,34 PLN</td>
+                <td>
+                  <a class="delete-icon" href="#">
+                    <img src="./assets/icons/shopping_cart/delete.svg" alt="delete" width="17" height="20">
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td class="table-number-col">
+                  <b>4</b>
+                </td>
+                <td class="table-product-col">
+                  <div class="table-product-box">
+                    <div class="table-product-box__top">
+                      <p>Produkt 4</p>
+                    </div>
+                    <div class="table-product-box__bottom">
+                      <div class="table-product-box__bottom--left">
+                        <img src="./assets/img/products_card/smartwatch.jpg" alt="img" width="71" height="71">
+                      </div>
+                      <div class="table-product-box__bottom--right">
+                        <p>Kod: <span>1333903</span>
+                        </p>
+                        <p>Kolor: <span>czarny</span>
+                        </p>
+                        <p>Marka: <span>Basic</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <input type="number" min="0" name="product-quantity" id="product-quantity">
+                </td>
                 <td>1,95 zł</td>
                 <td>1,09 PLN</td>
                 <td>1,34 PLN</td>
@@ -122,9 +194,33 @@
           </div>
         </div>
         <div class="summary-box__button">
-          <button class="color-btn">Wyślij zapytanie</button>
+          <a href="#wyslij-zapytanie" class="color-btn">wyślij zapytanie</a>
         </div>
       </aside>
     </div>
+    <form id="wyslij-zapytanie">
+        <h2>Wyslij zapytanie</h2>
+        <div class="form-inputs-row">
+          <div class="input-slot">
+            <label for="name-surname">Imię i nazwisko: *</label>
+            <input class="form-input" type="text" name="name-surname" id="name-surname" placeholder="Anna Kowalska" required>
+          </div>
+          <div class="input-slot">
+            <label for="email">Adres e-mail: *</label>
+            <input class="form-input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" id="email" placeholder="przykladowy@mail.com">
+          </div>
+          <div class="input-slot">
+            <label for="telephone">Numer telefonu:</label>
+            <input class="form-input" type="text" name="telephone" id="telephone" placeholder="np. +48 000 999 000">
+          </div>
+        </div>
+        <div class="input-slot">
+          <label for="msgarea">Treść wiadomości:</label>
+          <textarea class="form-textarea" name="msgarea" id="msgarea" placeholder="Treść zapytania"></textarea>
+        </div>
+        <div class="request-btn">
+          <button class="color-btn">wyślij</button>
+        </div>
+      </form>
   </div>
 </div>
